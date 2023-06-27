655ccc2af1526c6c457708829d5d18b6a9abf06b
[rachel-laws] <[rlawson13l@gmail.com]>
2023-06-26 23:28:28 -0400

    Compress images

--- dist/script.js
+++ dist/script.js
@@ -111,7 +111,6 @@ window.onload = function fadeInBody() {
   isLoaded = true;
   const body = document.body;
   body.style.opacity = 0;
-  body.style.animation = 'fadeIn 300ms ease 1';
 
   setTimeout(function () {
     body.style.opacity = 1;
