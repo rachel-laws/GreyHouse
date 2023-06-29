3117ddcb0480ac58412c02a5f1403d67c8078fe7
[rachel-laws] <[rlawson13l@gmail.com]>
2023-06-29 11:43:12 -0400

    Add hide navbar animation

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
-// Mobile Navigation
+//* Mobile Navigation
 
 const nav = document.querySelector('#navList');
+const navbar = document.querySelector('#navbar');
 const navLinks = document.querySelector('nav a');
 const mobileNavBtn = document.querySelector('#toggleMobileNav');
 const mobileNavBtnIcon = document.querySelector('#toggleMobileNav i');
@@ -8,17 +9,57 @@ const navLinkNotCurrent = document.querySelector(
   'a:not([aria-current="page"])'
 );
 
+// Sticky Navbar (for position: sticky bug fix)
+
+window.addEventListener('load', () => {
+  const navOffset = navbar.offsetTop;
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= navOffset) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  });
+});
+
+// Show / Hide Navbar
+
 mobileNavBtn.addEventListener('click', e => {
   if (nav.classList.contains('showMobileNav')) {
-    mobileNavBtn.setAttribute('aria-expanded', 'false');
-    mobileNavBtnIcon.classList.replace('bi-x-lg', 'bi-list');
-    nav.classList.remove('showMobileNav');
+    hideNavbar(nav);
   } else {
-    mobileNavBtn.setAttribute('aria-expanded', 'true');
-    mobileNavBtnIcon.classList.replace('bi-list', 'bi-x-lg');
-    nav.classList.add('showMobileNav');
+    showNavbar(nav);
   }
 });
+
+// Close when clicking outside navbar
+
+document.addEventListener('click', function handleClickOutsideBox(event) {
+  if (nav.classList.contains('showMobileNav')) {
+    if (!nav.contains(event.target) && !navbar.contains(event.target)) {
+      hideNavbar(nav);
+    }
+  }
+});
+
+// Helper Functions
+
+const hideNavbar = nav => {
+  mobileNavBtn.setAttribute('aria-expanded', 'false');
+  mobileNavBtnIcon.classList.replace('bi-x-lg', 'bi-list');
+  nav.classList.add('hideMobileNav');
+  setTimeout(() => {
+    nav.classList.remove('showMobileNav');
+    nav.classList.remove('hideMobileNav');
+  }, 150);
+};
+
+const showNavbar = nav => {
+  mobileNavBtn.setAttribute('aria-expanded', 'true');
+  mobileNavBtnIcon.classList.replace('bi-list', 'bi-x-lg');
+  nav.classList.add('showMobileNav');
+};
 
 // Active Page Underline
 
@@ -49,22 +90,9 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-// Sticky Navbar (for bug fix)
+//* Image Carousel
 
-window.addEventListener('load', () => {
-  const navbar = document.querySelector('#navbar');
-  const navOffset = navbar.offsetTop;
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY >= navOffset) {
-      navbar.classList.add('sticky');
-    } else {
-      navbar.classList.remove('sticky');
-    }
-  });
-});
-
-// Image Carousel
+// Preload
 
 function preloadImages(imageSources) {
   for (const source of imageSources) {
@@ -82,6 +110,8 @@ const headerImageSources = [
 ];
 
 preloadImages(headerImageSources);
+
+// Change Images
 
 function executeOnLargeScreen(callback) {
   if (window.innerWidth > 650) {
@@ -101,7 +131,7 @@ function changeHeaderImg() {
 
 executeOnLargeScreen(changeHeaderImg);
 
-// Contact Form Submission
+//* Contact Form Submission
 
 const contactForm = document.querySelector('#contactForm');
 const submitMessage = document.querySelector('#formMessage');
